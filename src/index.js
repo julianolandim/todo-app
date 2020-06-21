1829f7f9c5e85c21022660ae940c1bd122b05381
julianolandim <juliano@ibg.ind.br>
2020-06-21 13:33:08 -0300

    Adicionando reducers e actions

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TodoApp from "./TodoApp";
+import Provider from "react-redux/lib/components/Provider";
+import {createStore} from "redux";
 
 
-ReactDOM.render(<TodoApp />, document.getElementById('root'));
+const store = createStore(()=>[])
+
+ReactDOM.render(
+    <Provider store={store}>
+        <TodoApp/>
+    </Provider>,
+    document.getElementById('root'));
 
---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+
+const INITIAL_STATE = {
+    todos : [],
+    text : ""
+}
+
+function rootReducer(state = INITIAL_STATE, action) {
+    return state;
+}
+
+export default rootReducer()
