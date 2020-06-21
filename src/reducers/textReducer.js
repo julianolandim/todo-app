220f4fa0271544984f9648f2f674ab26ef61437a
julianolandim <juliano@ibg.ind.br>
2020-06-21 16:46:47 -0300

    Limpando o campo apos adicionar o TODO

--- src/reducers/textReducer.js
+++ src/reducers/textReducer.js
@@ -1,10 +1,12 @@
-import {SET_TEXT} from "../actions";
+import {SET_TEXT, ADD_TODO} from "../actions";
 
 
 function textReducer(state = "", action) {
     switch (action.type) {
         case SET_TEXT :
-            return action.text
+            return action.text;
+        case ADD_TODO:
+            return "";
         default :
             return state;
     }
